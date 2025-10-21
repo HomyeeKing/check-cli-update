0fe4bf8559b04894e02ca88d706dee55b4af63e8
HomyeeKing <HomyeeKing@gmail.com>
2025-10-21 20:31:02 +0800

    feat: allow registry

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { pkgUp } from "pkg-up"
-import fs from "fs"
-import { getLatestVersion } from "./npm"
+import fs from "fs";
+import { pkgUp } from "pkg-up";
+import { getLatestVersion } from "./npm";
 
 /**
  *
@@ -8,43 +8,49 @@ import { getLatestVersion } from "./npm"
  * @returns
  */
 export const getCWDPackageJson = async (cwd?: string) => {
-  const pjsonPath = (await pkgUp({ cwd }))!
-  return JSON.parse(fs.readFileSync(pjsonPath, { encoding: "utf-8" }))
-}
+  const pjsonPath = (await pkgUp({ cwd }))!;
+  return JSON.parse(fs.readFileSync(pjsonPath, { encoding: "utf-8" }));
+};
 
 export async function checkUpdate(options: {
   customTips?: (p: {
-    latestVersion: string
-    curVersion: string
-    pkgName: string
-  }) => void
+    latestVersion: string;
+    curVersion: string;
+    pkgName: string;
+  }) => void;
   /** the directory where your cli located*/
-  cwd: string
+  cwd: string;
+  /** optional npm registry url */
+  registry?: string;
 }) {
-  const { customTips, cwd } = options || {}
-  const pjson = await getCWDPackageJson(cwd)
-  const latestVersion = await getLatestVersion(pjson.name)
+  const {
+    customTips,
+    cwd,
+    registry = "https://registry.npmjs.org/",
+  } = options || {};
+  const pjson = await getCWDPackageJson(cwd);
+  const latestVersion = await getLatestVersion(pjson.name, registry);
   try {
     // @ts-expect-error need developer to install semver
-    const semver = (await import("semver")).default
+    const semver = (await import("semver")).default;
     if (latestVersion && semver.gt(latestVersion, pjson.version)) {
       if (customTips) {
         customTips({
           latestVersion,
           curVersion: pjson.version,
           pkgName: pjson.name,
-        })
+        });
       } else {
         console.warn(
           "\x1b[33m%s",
           `The latest version of ${pjson.name} is ${latestVersion} and you have ${pjson.version}. Update it now: npm i -g ${pjson.name}`,
-        )
+        );
       }
     }
   } catch (e: any) {
     if (e.code === "ERR_MODULE_NOT_FOUND") {
-      console.error("\x1B[31m%s", '"semver" not found. Did you install it?')
+      console.error("\x1B[31m%s", '"semver" not found. Did you install it?');
     }
-    throw e.message
+    throw e.message;
   }
 }
--- src/npm.ts
+++ src/npm.ts
@@ -2,12 +2,14 @@ import { exec } from "child_process"
 
 export async function getLatestVersion(
   pkgName: string,
+  registry?: string,
 ): Promise<string | undefined> {
   if (pkgName) {
     return new Promise((resolve, reject) => {
       try {
+        const registryArg = registry ? ` --registry=${registry}` : ""
         exec(
-          `npm show ${pkgName} version`,
+          `npm show ${pkgName} version${registryArg}`,
           {
             encoding: "utf-8",
           },
